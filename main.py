e5043ec6cecdc36962da5cd26854eb357ae311bf
yeyevtushenko <yehor803@gmail.com>
2024-01-03 23:39:06 +0200

    Меню

--- main.py
+++ main.py
@@ -47,32 +47,34 @@ while True:
     if choice == "1":
         group = input("Введіть назву групи: ")
         albums = input("Введіть альбоми (розділені комою): ").split(',')
-        add_group(music_library, group, albums)
+        add(music_library, group, albums)
 
     elif choice == "2":
         group = input("Введіть назву групи для видалення: ")
-        remove_group(music_library, group)
+        remove(music_library, group)
 
     elif choice == "3":
         group = input("Введіть назву групи для пошуку: ")
-        search_result = search_group(music_library, group)
+        search_result = search(music_library, group)
         print("Search Result:", search_result)
 
     elif choice == "4":
         group = input("Введіть назву групи для редагування альбомів: ")
         new_albums = input("Введіть нові альбоми (розділені комою): ").split(',')
-        edit_group(music_library, group, new_albums)
+        edit(music_library, group, new_albums)
 
     elif choice == "5":
         filename = input("Введіть назву файлу для збереження: ")
-        save_to_file(music_library, filename)
+        save(music_library, filename)
 
     elif choice == "6":
         filename = input("Введіть назву файлу для завантаження: ")
-        music_library = load_from_file(filename)
+        music_library = load(filename)
 
     elif choice == "0":
         break
 
     else:
         print("Невірний вибір. Будь ласка, введіть дійсну опцію.")
+
+
