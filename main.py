3aca488c2ccfd78c45166160ca315b89e0dcaaa7
yeyevtushenko <yehor803@gmail.com>
2024-01-03 23:37:35 +0200

    Збереження та завантаження

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ def remove(library, group):
 
 def search(library, group):
     return library.get(group, "Група не знайдена у бібліотеці.")
+
+def edit(library, group, new_albums):
+    if group in library:
+        library[group] = new_albums
+    else:
+        print(f"Групи {group} немає у бібліотеці.")
+
+def save(library, filename):
+    with gzip.open(filename, 'wt') as file:
+        json.dump(library, file)
+
+def load(filename):
+    with gzip.open(filename, 'rt') as file:
+        return json.load(file)
