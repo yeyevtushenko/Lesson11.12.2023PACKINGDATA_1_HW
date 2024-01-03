4b7e5c44989f65e7f6cf9afb539c4484b9560372
yeyevtushenko <yehor803@gmail.com>
2024-01-03 23:09:53 +0200

    Видалення значень з словника

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ def add(capitals, country, capital):
     capitals[country] = capital
     print(f"Додано: {country} - {capital}")
 
+def remove(capitals, country):
+    if country in capitals:
+        del capitals[country]
+        print(f"Видалено: {country}")
+    else:
+        print(f"Країна {country} не знайдена")
+
+
+
 
