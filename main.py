812cdbf507bc98419157234de45a7d554ea13202
yeyevtushenko <yehor803@gmail.com>
2024-01-03 23:14:49 +0200

    Маємо певний словник з назвами країн і столиць. Назва країни використовується як ключ, назва столиці — як значення. Реалізуйте: додавання, видалення, пошук, редагування, збереження та завантаження даних (використовуючи стиснення та розпакування).

--- main.py
+++ main.py
@@ -50,7 +50,42 @@ def print_menu():
     print("6. Завантажити дані")
     print("0. Вийти")
 
+if __name__ == "__main__":
+    capitals_dict = load('capitals.json.gz')
 
+    while True:
+        print_menu()
+        choice = input("Ваш вибір: ")
+
+        if choice == "1":
+            country = input("Введіть назву країни: ")
+            capital = input("Введіть назву столиці: ")
+            add(capitals_dict, country, capital)
+
+        elif choice == "2":
+            country = input("Введіть назву країни для видалення: ")
+            remove(capitals_dict, country)
+
+        elif choice == "3":
+            country = input("Введіть назву країни для пошуку: ")
+            search(capitals_dict, country)
+
+        elif choice == "4":
+            country = input("Введіть назву країни для редагування: ")
+            new_capital = input("Введіть нову назву столиці: ")
+            edit(capitals_dict, country, new_capital)
+
+        elif choice == "5":
+            save(capitals_dict, 'capitals_updated.json.gz')
+
+        elif choice == "6":
+            capitals_dict = load('capitals_updated.json.gz')
+
+        elif choice == "0":
+            break
+
+        else:
+            print("Невірний вибір. Будь ласка, виберіть іншу опцію.")
 
 
 
