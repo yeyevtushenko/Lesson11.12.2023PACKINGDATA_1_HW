c76ef05a0ffe4ba31ffc928cc707192bc92e7e73
yeyevtushenko <yehor803@gmail.com>
2024-01-03 23:38:09 +0200

    Меню

--- main.py
+++ main.py
@@ -29,3 +29,50 @@ def save(library, filename):
 def load(filename):
     with gzip.open(filename, 'rt') as file:
         return json.load(file)
+
+music_library = {}
+
+while True:
+    print("\nМеню:")
+    print("1. Додати групу та альбоми")
+    print("2. Видалити групу")
+    print("3. Пошук групи")
+    print("4. Редагувати альбоми групи")
+    print("5. Зберегти у файл")
+    print("6. Завантажити з файлу")
+    print("0. Вийти")
+
+    choice = input("Введіть свій вибір: ")
+
+    if choice == "1":
+        group = input("Введіть назву групи: ")
+        albums = input("Введіть альбоми (розділені комою): ").split(',')
+        add_group(music_library, group, albums)
+
+    elif choice == "2":
+        group = input("Введіть назву групи для видалення: ")
+        remove_group(music_library, group)
+
+    elif choice == "3":
+        group = input("Введіть назву групи для пошуку: ")
+        search_result = search_group(music_library, group)
+        print("Search Result:", search_result)
+
+    elif choice == "4":
+        group = input("Введіть назву групи для редагування альбомів: ")
+        new_albums = input("Введіть нові альбоми (розділені комою): ").split(',')
+        edit_group(music_library, group, new_albums)
+
+    elif choice == "5":
+        filename = input("Введіть назву файлу для збереження: ")
+        save_to_file(music_library, filename)
+
+    elif choice == "6":
+        filename = input("Введіть назву файлу для завантаження: ")
+        music_library = load_from_file(filename)
+
+    elif choice == "0":
+        break
+
+    else:
+        print("Невірний вибір. Будь ласка, введіть дійсну опцію.")
